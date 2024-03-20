e63733fc2dd98827e09aab0acb77e6d7db5f6b52
Elie <jzy_dt@qq.com>
2024-03-20 17:13:22 +0800

    Initial commit

---
+++ python/boxplot.py
@@ -0,0 +1,33 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# 导入数据
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# np.random.seed(0)
+# data=np.random.normal(size=(100,))
+data = np.array(arr)
+
+# 计算5个值
+# 计算第25、50和75百分位数
+p25 = np.percentile(data, 25)
+p50 = np.percentile(data, 50)
+p75 = np.percentile(data, 75)
+# 计算最大值最小值
+Max = data.max()
+Min = data.min()
+
+print("最小值：",Min)
+print("第25百分位数：", p25)
+print("第50百分位数：", p50)
+print("第75百分位数：", p75)
+print("最大值：",Max)
+
+# 绘制箱线图
+plt.boxplot(data)
+
+# 添加标题和标签
+plt.title("Boxplot")
+plt.xlabel("Data")
+
+# 显示图形
+plt.show()
